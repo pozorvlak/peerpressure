d2cfc792a5505570f2874063e05f4305672ec1c5
Miles Gould <miles@assyrian.org.uk>
2010-11-25 11:50:07 +0000

    Created program

---
+++ rtern.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+# Script written for the short story "First Contact".
+
+use strict;
+use warnings;
+use 5.010;
+
+# Reverse ternary representation
+sub rtern {
+    my $num = shift;
+    my @digits;
+    while ($num > 0) {
+        my $digit = $num % 3;
+        push @digits, $digit;
+        $num = ($num - $digit) / 3;
+    }
+    return @digits;
+}
+
+while (<>) {
+    say rtern($_);
+}
